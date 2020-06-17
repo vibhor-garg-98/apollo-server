6210631c83cfd8cadc6c44664fb86f1ace89eabe
vibhor-garg-98 <vibhor.garg@successive.tech>
2020-06-17 16:59:54 +0530

    feat: Querying the list(feature/58720)

--- src/modules/trainee/query.js
+++ src/modules/trainee/query.js
@@ -2,16 +2,16 @@ import { UserInputError } from 'apollo-server';
 
 export default {
   getTrainee: async (parent, args, context) => {
-    try {
+    // try {
       const {
         dataSources: { traineeApi },
       } = context;
       const response = await traineeApi.getTrainee(args);
-      return response.data.records;
-    } catch (error) {
-      return new UserInputError('Arguments are invalid', {
-        invalidArgs: Object.keys(args),
-      });
-    }
+      return response.data;
+    // } catch (error) {
+    //   return new UserInputError('Arguments are invalid', {
+    //     invalidArgs: Object.keys(args),
+    //   });
+    // }
   },
 };
